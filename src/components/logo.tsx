f28f9eac337a4227be07263e44fdcf11faa57634
Tamás Majoros <tmsmajoros@gmail.com>
2025-08-25 18:37:42 +0000

    a logó file neve logo.svg

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Logo() {
   return (
     <div className="flex items-center gap-2">
-      <Image src="/logo.png" alt="VillanyPro Logo" width={24} height={24} className="h-6 w-6" />
+      <Image src="/logo.svg" alt="VillanyPro Logo" width={24} height={24} className="h-6 w-6" />
       <span className="font-bold font-headline text-xl">VillanyPro</span>
     </div>
   );
